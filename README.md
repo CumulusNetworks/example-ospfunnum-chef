605b515d0a97746cff90e06fe25958c87c7c6654
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-04-15 14:59:02 +0100

    Import Cookbooks from cldemo packages
    Import the cldemo_base & cldemo_ospfunnumbered cookbooks.
    Add a Gemfile & Cheffile for librarian-chef to install the 3rd party sysctl
    cookbook from Chef Supermarket.
    Import the nodes & data_bags for both 2s & 2lt22s demos

--- README.md
+++
@@ -1,1 +0,0 @@
-# example-ospfunnum-chef
---
+++ cookbooks/cldemo_base/metadata.rb
@@ -0,0 +1,9 @@
+name             'cldemo_base'
+maintainer       'Cumulus Networks'
+maintainer_email 'mwillsher@cumulusnetworks.com'
+license          'All rights reserved'
+description      'Installs/Configures cldemo_base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
+
+depends          'sysctl'
