fdb1508ec61ae8afdf5f131e52c1b09a47b28bb5
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-06-17 12:01:36 +0100

    Bump versions on modified cookbooks

--- site-cookbooks/cldemo_base/metadata.rb
+++ site-cookbooks/cldemo_base/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'mwillsher@cumulusnetworks.com'
 license          'All rights reserved'
 description      'Installs/Configures cldemo_base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
